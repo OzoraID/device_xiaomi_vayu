9c4b1675be04a6a74db4448713decd3ed374f206
Adithya R <gh0strider.2k18.reborn@gmail.com>
2021-08-18 12:43:50 +0530

    vayu: parts: Add back Touch Profiles for gaming and benchmarking
    Change-Id: I5f0cc9ddf5a9e7e4ffb0ed0a6df51c8a57714c59

--- parts/src/org/lineageos/settings/thermal/ThermalSettingsFragment.java
+++ parts/src/org/lineageos/settings/thermal/ThermalSettingsFragment.java
@@ -16,6 +16,7 @@
 package org.lineageos.settings.thermal;
 
 import android.annotation.Nullable;
+import android.app.ActionBar;
 import android.content.Context;
 import android.content.Intent;
 import android.content.pm.ApplicationInfo;
@@ -101,6 +102,8 @@ public class ThermalSettingsFragment extends PreferenceFragment
     public void onResume() {
         super.onResume();
         getActivity().setTitle(getResources().getString(R.string.thermal_title));
+        final ActionBar actionBar = getActivity().getActionBar();
+        actionBar.setTitle(getResources().getString(R.string.thermal_title));
         rebuild();
     }
 
@@ -218,6 +221,7 @@ public class ThermalSettingsFragment extends PreferenceFragment
         private ImageView icon;
         private View rootView;
         private ImageView stateIcon;
+        private ImageView touchIcon;
 
         private ViewHolder(View view) {
             super(view);
@@ -225,6 +229,7 @@ public class ThermalSettingsFragment extends PreferenceFragment
             this.mode = view.findViewById(R.id.app_mode);
             this.icon = view.findViewById(R.id.app_icon);
             this.stateIcon = view.findViewById(R.id.state);
+            this.touchIcon = view.findViewById(R.id.touch);
             this.rootView = view;
 
             view.setTag(this);
@@ -317,6 +322,21 @@ public class ThermalSettingsFragment extends PreferenceFragment
 
             holder.mode.setAdapter(new ModeAdapter(context));
             holder.mode.setOnItemSelectedListener(this);
+            holder.touchIcon.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    TouchSettingsFragment touchSettingsFragment = new TouchSettingsFragment();
+                    Bundle bundle = new Bundle();
+                    bundle.putString("appName", entry.label);
+                    bundle.putString("packageName", entry.info.packageName);
+                    touchSettingsFragment.setArguments(bundle);
+                    getActivity().getFragmentManager().beginTransaction()
+                            .replace(android.R.id.content, touchSettingsFragment, "touchSettingsFragment")
+                            .addToBackStack(null)
+                            .commit();
+                }
+            });
+
             holder.title.setText(entry.label);
             holder.title.setOnClickListener(v -> holder.mode.performClick());
             mApplicationsState.ensureIcon(entry);
@@ -325,6 +345,16 @@ public class ThermalSettingsFragment extends PreferenceFragment
             holder.mode.setSelection(packageState, false);
             holder.mode.setTag(entry);
             holder.stateIcon.setImageResource(getStateDrawable(packageState));
+            int stateIconDawable = getStateDrawable(mThermalUtils.getStateForPackage(
+                    entry.info.packageName));
+            if (stateIconDawable == R.drawable.ic_thermal_gaming ||
+                    stateIconDawable == R.drawable.ic_thermal_benchmark) {
+                holder.touchIcon.setVisibility(View.VISIBLE);
+            } else {
+                holder.touchIcon.setVisibility(View.GONE);
+            }
+            holder.stateIcon.setImageResource(stateIconDawable);
+            return holder.rootView;
         }
 
         private void setEntries(List<ApplicationsState.AppEntry> entries,
