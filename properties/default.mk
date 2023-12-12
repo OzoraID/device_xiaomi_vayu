2754d5a3a48df949161a7c06c9390ca69fc4613e
NekoTuru10 <kirmansyah9696@gmail.com>
2023-12-12 19:18:03 +0700

    import from aospa

--- properties/default.mk
+++ properties/default.mk
@@ -15,19 +15,12 @@
 #
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
+    PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     debug.sf.frame_rate_multiple_threshold=120 \
-    ro.surface_flinger.max_virtual_display_dimension=4096 \
-    ro.surface_flinger.has_HDR_display=true \
-    ro.surface_flinger.has_wide_color_display=false \
-    ro.surface_flinger.max_frame_buffer_acquired_buffers=3 \
-    ro.surface_flinger.protected_contents=true \
     ro.surface_flinger.set_display_power_timer_ms=1000 \
     ro.surface_flinger.set_idle_timer_ms=80 \
     ro.surface_flinger.set_touch_timer_ms=200 \
-    ro.surface_flinger.use_color_management=true \
-    ro.surface_flinger.use_content_detection_for_refresh_rate=true \
-    vendor.display.idle_time=0 \
-    vendor.display.idle_time_inactive=0
+    ro.surface_flinger.use_content_detection_for_refresh_rate=true
 
 # Enable app/sf phase offset as durations. The numbers below are translated from the existing
 # positive offsets by finding the duration app/sf will have with the offsets.
@@ -38,14 +31,14 @@ PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
 # https://docs.google.com/spreadsheets/d/1a_5cVNY3LUAkeg-yL56rYQNwved6Hy-dvEcKSxp6f8k/edit
 
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
-    debug.sf.early_app_phase_offset_ns=500000 \
-    debug.sf.early_gl_app_phase_offset_ns=15000000 \
-    debug.sf.early_gl_phase_offset_ns=3000000 \
-    debug.sf.early_phase_offset_ns=500000 \
-    debug.sf.high_fps_early_gl_phase_offset_ns=6500000 \
-    debug.sf.high_fps_early_phase_offset_ns=6100000 \
-    debug.sf.high_fps_late_app_phase_offset_ns=1000000 \
-    debug.sf.phase_offset_threshold_for_next_vsync_ns=12100000
+    debug.sf.use_phase_offsets_as_durations=1 \
+    debug.sf.late.sf.duration=10500000 \
+    debug.sf.late.app.duration=16600000 \
+    debug.sf.early.sf.duration=16600000 \
+    debug.sf.early.app.duration=16600000 \
+    debug.sf.earlyGl.sf.duration=16600000 \
+    debug.sf.earlyGl.app.duration=16600000
+
 ifneq ($(TARGET_BUILD_VARIANT),eng)
 PRODUCT_SYSTEM_DEFAULT_PROPERTIES += \
     pm.dexopt.boot=verify \
