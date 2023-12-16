63c92d36e2b92ed91be982acfdb716b86b0e372c
NekoTuru10 <kirmansyah9696@gmail.com>
2023-12-16 19:57:48 +0000

    Revert "vayu: libqti-perfd-client: Move to C"
    This reverts commit 22b9fc7b557ac1542ca08c543683c3eb95c2de44.

--- libqti-perfd-client/client.cpp
+++ libqti-perfd-client/client.cpp
@@ -6,13 +6,6 @@
 
 #include <stdint.h>
 
-void perf_get_feedback() {}
-void perf_hint() {}
-void perf_lock_acq() {}
-void perf_lock_cmd() {}
-void perf_lock_rel() {}
-void perf_lock_use_profile() {}
-
 extern "C" void perf_get_feedback() {}
 extern "C" void perf_hint() {}
 extern "C" void perf_lock_acq() {}
