794f2fcd6d932c26531d871186517b0f1721ccff
NekoTuru10 <kirmansyah9696@gmail.com>
2023-12-16 18:59:03 +0000

    miss

--- libqti-perfd-client/client.c
+++ libqti-perfd-client/client.c
@@ -13,9 +13,3 @@ void perf_lock_cmd() {}
 void perf_lock_rel() {}
 void perf_lock_use_profile() {}
 
-extern "C" void perf_get_feedback() {}
-extern "C" void perf_hint() {}
-extern "C" void perf_lock_acq() {}
-extern "C" void perf_lock_cmd() {}
-extern "C" void perf_lock_rel() {}
-extern "C" void perf_lock_use_profile() {}
